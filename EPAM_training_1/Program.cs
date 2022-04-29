1ba3cde8f0dc025717da6c0d25965420ca661876
Georgi Bachilo <goshasbachilo@gmail.com>
2022-04-29 18:48:00 +0300

    Add project files.

---
+++ EPAM_training_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EPAM_training_1
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+
+			int a = 0;
+			int max = 0;
+
+			string b = Console.ReadLine();
+
+		//	string s = new string("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard");
+
+			for (int i = 0; i < b.Length - 1; i++)
+			{
+
+				if (b[i] == b[i + 1])
+				{
+					a++;
+
+					if (a > max)
+					{
+						max = a + 1;
+					}
+				}
+				else
+                {
+					a = 0;
+                }
+			}
+			Console.WriteLine(max);
+
+
+		}
+	}
+}
